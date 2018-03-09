73731f60df8afa4e01a61a87a5fd3158f4449d72
Charles <dailiang18bb@gmail.com>
2018-03-08 22:49:37 -0500

    Solutions update
    first 8 solutions update

---
+++ HackerRank_Java/Code/src/introduction_1_Java_WelcomeToJava/Solution.java
@@ -0,0 +1,13 @@
+package introduction_1_Java_WelcomeToJava;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		/*
+		 * Enter your code here. Print output to STDOUT. Your class should be named
+		 * Solution.
+		 */
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+}
