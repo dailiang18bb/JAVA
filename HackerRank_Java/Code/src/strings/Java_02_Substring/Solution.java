354f3fbd31bada218d5041d5cf2538c40e45a043
Charles <dailiang18bb@gmail.com>
2018-03-14 21:42:31 -0400

    Question2 update

---
+++ HackerRank_Java/Code/src/strings/Java_02_Substring/Solution.java
@@ -0,0 +1,20 @@
+package strings.Java_02_Substring;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		int start = in.nextInt();
+		int end = in.nextInt();
+		
+		//my code
+		System.out.println(S.substring(start, end));
+	}
+}
