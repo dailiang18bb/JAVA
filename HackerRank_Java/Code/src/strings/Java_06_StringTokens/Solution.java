7a89963d43225f6f765c9306b47b14671fe9a9f0
Charles <dailiang18bb@gmail.com>
2018-03-16 08:55:10 -0400

    question6 start

---
+++ HackerRank_Java/Code/src/strings/Java_06_StringTokens/Solution.java
@@ -0,0 +1,17 @@
+package strings.Java_06_StringTokens;
+
+import java.util.ArrayList;
+
+public class Solution {
+	
+	
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> test = new ArrayList<>();
+		test.add(1);
+		
+		
+		
+	}
+
+}
