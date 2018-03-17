025edb185a37ba6ef381567be87d2112db545375
Charles <dailiang18bb@gmail.com>
2018-03-17 10:56:30 -0400

    perfect my code

--- HackerRank_Java/Code/src/strings/Java_07_PatternSyntaxChecker/Solution.java
+++ HackerRank_Java/Code/src/strings/Java_07_PatternSyntaxChecker/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
-		while (testCases > 0) {
+		while (testCases-- > 0) {
 			String pattern = in.nextLine();
 			// Write your code
 			try {
@@ -16,7 +16,32 @@ public class Solution {
 			} catch (PatternSyntaxException e) {
 				System.out.println("Invalid");
 			}
-
 		}
 	}
 }
+
+
+/*
+ * Instructor Solution
+ * 
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            testCases--;
+            try{
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }catch(PatternSyntaxException ex){
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
+*/
