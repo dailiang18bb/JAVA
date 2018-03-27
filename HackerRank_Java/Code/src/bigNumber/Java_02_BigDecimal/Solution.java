ea13d4846b2f2fde1a5f6b2baf8ade8a07bc0baa
Charles <dailiang18bb@gmail.com>
2018-03-27 17:52:32 -0400

    override compare

---
+++ HackerRank_Java/Code/src/bigNumber/Java_02_BigDecimal/Solution.java
@@ -0,0 +1,47 @@
+package bigNumber.Java_02_BigDecimal;
+
+
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+		// Write your code here
+        
+        Arrays.sort(s,0,n, Collections.reverseOrder(new Comparator<String>() {
+            @Override
+            public int compare(String a1, String a2) {
+                //convert to big decimal inside comparator
+                //so permanent string values are never changed
+                //aka you only use the BigDecimal values to 
+                //compare the strings!
+                BigDecimal a = new BigDecimal(a1);
+                BigDecimal b = new BigDecimal(a2);
+                return a.compareTo(b);
+            }
+        }));
+        
+        
+        
+       
+        
+		
+		
+		
+		
+		
+		
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
