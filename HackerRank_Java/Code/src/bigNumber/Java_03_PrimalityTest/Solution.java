07b02ebc8f625d1f1af40e1d31386ba27966514f
Charles <dailiang18bb@gmail.com>
2018-03-27 18:50:38 -0400

    ...

---
+++ HackerRank_Java/Code/src/bigNumber/Java_03_PrimalityTest/Solution.java
@@ -0,0 +1,20 @@
+package bigNumber.Java_03_PrimalityTest;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		BigInteger n = in.nextBigInteger();
+		in.close();
+		
+		// Write your code here.
+		System.out.println(n.isProbablePrime(1) ? "prime" : "not prime");
+
+	}
+}
