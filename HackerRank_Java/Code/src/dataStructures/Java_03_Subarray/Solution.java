8fa5a88ffc8454890af03e0f6763f6d085c979a1
Charles <dailiang18bb@gmail.com>
2018-03-31 17:46:48 -0400

    q3 update

---
+++ HackerRank_Java/Code/src/dataStructures/Java_03_Subarray/Solution.java
@@ -0,0 +1,39 @@
+package dataStructures.Java_03_Subarray;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	private static int sumArr(int[] array) {
+		int sum = 0;
+		for (int num : array) {
+			sum = sum + num;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+		 * class should be named Solution.
+		 */
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = in.nextInt();
+		}
+
+		int countNegativeSum = 0;
+		int[] tempArr = new int[n];
+
+		for (int i = 0; i < n + 1; i++) {
+			for (int j = i + 1; j < n + 1; j++) {
+				tempArr = Arrays.copyOfRange(arr, i, j);
+				countNegativeSum = sumArr(tempArr) < 0 ? countNegativeSum + 1 : countNegativeSum;
+			}
+		}
+		System.out.println(countNegativeSum);
+	}
+}
