a945f0c03ffed54c2a9b45e3d722afaf4578321d
Charles <dailiang18bb@gmail.com>
2018-04-06 17:24:26 -0400

    ...

--- HackerRank_Java/Code/src/dataStructures/Java_07_Map/Solution.java
+++ HackerRank_Java/Code/src/dataStructures/Java_07_Map/Solution.java
@@ -6,16 +6,29 @@ import java.io.*;
 
 class Solution {
 	public static void main(String[] argh) {
+
+		// map
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
+
 		in.nextLine();
 		for (int i = 0; i < n; i++) {
 			String name = in.nextLine();
 			int phone = in.nextInt();
 			in.nextLine();
+			map.put(name, phone);
 		}
 		while (in.hasNext()) {
 			String s = in.nextLine();
+			if (map.containsKey(s)) {
+				System.out.println(s + "=" + map.get(s));
+			} else {
+				System.out.println("Not found");
+			}
 		}
+		in.close();
 	}
 }
---
+++ HackerRank_Java/Code/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Map;
+import java.util.jar.Attributes.Name;
+
+import javax.net.ssl.HandshakeCompletedEvent;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Map<String, Integer> Name = new Map<String, Integer>();
+
+	}
+
+}
