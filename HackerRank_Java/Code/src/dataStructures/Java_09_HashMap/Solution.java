19327e8f0fea316c1f3309f780930849d0d4b45e
Charles <dailiang18bb@gmail.com>
2018-04-09 19:33:56 -0400

    HashSet

---
+++ HackerRank_Java/Code/src/dataStructures/Java_09_HashMap/Solution.java
@@ -0,0 +1,30 @@
+package dataStructures.Java_09_HashMap;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		String[] pair_left = new String[t];
+		String[] pair_right = new String[t];
+
+		for (int i = 0; i < t; i++) {
+			pair_left[i] = s.next();
+			pair_right[i] = s.next();
+		}
+		// Write your code here
+		s.close();
+		HashSet<String> hSet = new HashSet<String>();
+		for (int i = 0; i < t; i++) {
+			hSet.add(pair_left[i] + " " + pair_right[i]);
+			System.out.println(hSet.size());
+		}
+		// end my code here
+	}
+}
