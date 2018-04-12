21b475c6abbd13952272e1ef1460e94276d17b12
Charles <dailiang18bb@gmail.com>
2018-04-12 18:11:24 -0400

    ...

--- HackerRank_Java/Code/src/test/Test.java
+++ HackerRank_Java/Code/src/test/Test.java
@@ -10,6 +10,8 @@ import javax.net.ssl.HandshakeCompletedEvent;
 public class Test {
 
 	public static void main(String[] args) {
+		String[] a= {"abc","def"};
+		System.out.println(a[1].compareTo(a[0]));
 		
 		
 	}
