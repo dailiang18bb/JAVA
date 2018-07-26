1d18ca6ebd4af8a83d8ca8917b0baefd14d8ba47
Charles <dailiang18bb@gmail.com>
2018-07-26 11:52:09 -0400

    Binary Go

--- HackerRank_Java/Code/src/test/temp.java
+++ HackerRank_Java/Code/src/test/temp.java
@@ -1,34 +1,96 @@
+package test;
 
-//Complete this code or write your own from scratch
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
-class Solution {
 
-	private static Map<String, Integer> phoneBook = new HashMap<>();
+public class temp {
 
-	public static void printName(String name) {
-		if (phoneBook.containsKey(name)) {
-			System.out.println(name + "=" + phoneBook.get(name));
-		} else {
-			System.out.println("Not found");
+	/**
+	 * findConsecutiveNumber P,
+	 * 
+	 * @param n
+	 *            the ArrayList
+	 * @param p
+	 *            the Consecutive number
+	 * @return the max Consecutive number
+	 */
+	// n = [1,0,0,0,0,1,0,0]
+	// p = 0
+	// return 4;
+	public static int findConsecutiveNumber(ArrayList<Integer> n, int p) {
+		int maxLength = 0;
+		int tempLength = 0;
+		for (int i = 0; i < n.size(); i++) {
+//			if (n.get(i) == p) {
+//				tempLength++;
+//			} else {
+//				tempLength = 0;
+//			}
+			tempLength = (n.get(i) == p)? tempLength + 1 : 0;
+			maxLength = (tempLength > maxLength) ? tempLength : maxLength;
 		}
+		return maxLength;
 	}
+	
+	
+	/**
+	 * convert a base10 integer to a base2 ArrayList
+	 * 
+	 * @param n,
+	 *            the base10 integer
+	 * @return the base2 ArrayList
+	 */
+	// 7 --> 111
+	public static ArrayList<Integer> base10ToBase2(int n) {
+		ArrayList<Integer> b2ArrayList = new ArrayList();
+		if (n == 0) {
+			b2ArrayList.add(0);
+		}
+		while (n != 0) {
+			b2ArrayList.add(n % 2);
+			n = n / 2;
+		}
+		Collections.reverse(b2ArrayList);
+		return b2ArrayList;
+	}
+	
+	public static String base10ToBase2String(int n) {
+		return Integer.toString(n, 2);
+	}
+	
+	
+	public static int findConsecutiveNumberByString(String strBinary, int p) {
+		String REGAX_INITIATE = "[0123456789]";
+		String Regex = REGAX_INITIATE.substring(0, p + 1) + REGAX_INITIATE.substring(p + 2);
+		System.out.println(Regex);
+		String[] strparts = strBinary.split(Regex);
+		int maxLength = 0;
+		for (int i = 0; i < strparts.length; i++) {
+			if (maxLength < strparts[i].length()) {
+				maxLength = strparts[i].length();
+			}
+		}
+		return maxLength;
+	}
+	
+	
 
-	public static void main(String[] argh) {
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		for (int i = 0; i < n; i++) {
-			String name = in.next();
-			int phone = in.nextInt();
-			// Write code here
-			phoneBook.put(name, phone);
-		}
-		while (in.hasNext()) {
-			String s = in.next();
-			// Write code here
-			printName(s);
-		}
-		in.close();
+		System.out.println(findConsecutiveNumberByString("10101010100011110100101010013245802", 1));
+		
+	
+		
+		//System.out.println(strparts[5]);
+//		String strBinary = Integer.toString(n, 2);
+//		System.out.println(strBinary);
+//		   // above is binary of given in in string
+//		String[] strparts = strBinary.split("0");
+//		for(String x: strparts) {
+//			System.out.println(x);
+//		}
 	}
 }
