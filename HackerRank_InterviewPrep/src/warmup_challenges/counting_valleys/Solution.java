527326430262b33f6f9ff549327ca7dc7ca5054e
Charles <dailiang18bb@gmail.com>
2019-03-17 15:06:53 -0400

    Counting Valleys updated
    2nd

---
+++ HackerRank_InterviewPrep/src/warmup_challenges/counting_valleys/Solution.java
@@ -0,0 +1,53 @@
+package warmup_challenges.counting_valleys;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        int result = 0;
+        Stack<Character> myStack = new Stack<>();
+        char[] ch = s.toCharArray();
+        for(Character x : ch){
+            if(myStack.empty()||myStack.peek() == x){
+                myStack.add(x);
+            } else{
+                myStack.pop();
+                if(myStack.empty() && x == 'U'){
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    
+    
+    //
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
